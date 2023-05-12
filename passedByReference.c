871db153848d4d023d983a37b5bf2547c8d60f6d
TomasNagy <tomi.nagy@outlook.com>
2023-05-12 13:57:16 +0200

    Add passedByReference.c

---
+++ passedByReference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int callMyFunc(int variable[]);
+
+int main(void)
+{
+	int array[2] = {1, 2}; 	
+
+	int x = callMyFunc(array); 
+	
+	// The output should be x=2, array[0]=1, array[1]=5
+	printf("x=%i, array[0]=%i, array[1]=%i", x, array[0], array[1]);
+}
+
+int callMyFunc(int variable[])
+{
+	variable[1] = 5;
+	// In return the variable's value is not modified
+	return variable[0]++;
+}
