72616ab6d67d43109661b80d3f4d548a5606b002
TomasNagy <tomi.nagy@outlook.com>
2023-05-12 13:59:53 +0200

    Add comments to passedByReference.c

--- passedByReference.c
+++ passedByReference.c
@@ -6,6 +6,7 @@ int main(void)
 {
 	int array[2] = {1, 2}; 	
 
+	//CAUTION! array is passed by reference
 	int x = callMyFunc(array); 
 	
 	// The output should be x=2, array[0]=1, array[1]=5
@@ -13,7 +14,8 @@ int main(void)
 }
 
 int callMyFunc(int variable[])
-{
+{	
+	// variable[1] is array[1]
 	variable[1] = 5;
 	// In return the variable's value is not modified
 	return variable[0]++;
